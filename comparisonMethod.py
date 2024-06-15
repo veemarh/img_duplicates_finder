46759aaa0aecb544211d82d0a6fd300c492e4330
xeniaTa <kseniatarsenko1@gmail.com>
2024-06-15 13:39:33 +0300

    add class ComparisonMethod

---
+++ comparisonMethod.py
@@ -0,0 +1,7 @@
+class ComparisonMethod:
+    def __init__(self):
+        self.name = 'aHash'
+        self.hash_size = 16
+        self.bhash_quick = False
+        self.comparison_size = 16
+        self.similarity = 100
