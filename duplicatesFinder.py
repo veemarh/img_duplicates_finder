891fe4cf6fce4ed3cc86561a18f6ce9adfb88847
xeniaTa <kseniatarsenko1@gmail.com>
2024-06-16 17:14:18 +0300

    add __action_with_duplicates

--- duplicatesFinder.py
+++ duplicatesFinder.py
@@ -15,7 +15,7 @@ from comparisonObject import ComparisonObject
 # 4 - reflected horizontally,
 # 5 - reflected vertically,
 # 6 - reflected horizontally and rotated 90 degrees to the right,
-# 7 - reflected vertically and rotated 90 degrees to the left
+# 7 - reflected vertically and rotated 90 degrees to the right
 class DuplicatesFinder:
     def __init__(self, comparison_method: ComparisonMethod):
         self.files = []
@@ -52,23 +52,12 @@ class DuplicatesFinder:
                     
                     curr_obj = ComparisonObject(path_curr_img, comparison_method)
 
-                    if is_duplicates(checked_obj.comparison_data, curr_obj.comparison_data, comparison_method):
-                        print(f"{checked_obj.file_path} - {path_curr_img}")
-                        name_curr_img = os.path.basename(path_curr_img)
-                        if self.folder_for_move:
-                            Path(path_curr_img).rename(f"{self.folder_for_move}/{name_curr_img}")
+                    if is_duplicates(checked_obj.comparison_data, curr_obj.comparison_data, comparison_method) or \
+                        (self.search_modified_images and check_modified(checked_obj, curr_obj, comparison_method, self.modified_images_properties)):
+                        self.__action_with_duplicates(checked_obj, curr_obj)
                         del paths_images[curr_i]
                         duplicate_count += 1
                     else:
-                        if self.search_modified_images:
-                            if check_modified(checked_obj, curr_obj, comparison_method, self.modified_images_properties):
-                                print(f"{checked_obj.file_path} - {path_curr_img}")
-                                name_curr_img = os.path.basename(path_curr_img)
-                                if self.folder_for_move:
-                                    Path(path_curr_img).rename(f"{self.folder_for_move}/{name_curr_img}")
-                                del paths_images[curr_i]
-                                duplicate_count += 1
-                                continue
                         curr_i += 1
                         
             check_i += 1
@@ -108,4 +97,12 @@ class DuplicatesFinder:
                                 
     #     print(duplicate_count, 'duplicates found')
     #     print(f'Script running time: {time.monotonic() - start}')
+    
+    @protected
+    def __action_with_duplicates(self, checked_obj: ComparisonObject, curr_obj: ComparisonObject):
+        path_curr_img = curr_obj.file_path
+        print(f"{checked_obj.file_path} - {path_curr_img}")
+        name_curr_img = os.path.basename(path_curr_img)
+        if self.folder_for_move:
+            Path(path_curr_img).rename(f"{self.folder_for_move}/{name_curr_img}")
         
