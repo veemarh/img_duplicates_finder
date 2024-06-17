16ae5c069d31eb5dd312b25ccaa390059919e4cd
xeniaTa <kseniatarsenko1@gmail.com>
2024-06-17 12:37:18 +0300

    delete __find

--- duplicates_finder/duplicatesFinder.py
+++ duplicates_finder/duplicatesFinder.py
@@ -8,7 +8,8 @@ from duplicates_finder.comparisonMethod import ComparisonMethod
 from duplicates_finder.comparisonObject import ComparisonObject
 
 # var 'bhash_quick' only for bhash - parameter 'Fast' = True, 'Precise' = False
-# var 'compare_size' for bhash and mhash - parameter 'Comparison area' (bhash:128x128, 256x256, 512x512; mhash: 8x8, 16x16 )  
+# var 'compare_size' for bhash and mhash 
+# - parameter 'Comparison area' (bhash:128x128, 256x256, 512x512; mhash: 8x8, 16x16 )  
 # var 'modified_images_properties' has keys:
 # 1 - rotated 90 deg to the right,
 # 2 - rotated 180 deg,
@@ -23,80 +24,73 @@ class DuplicatesFinder:
         self.specified_file = None
         self.require_identical_properties =  False
         self.identical_properties = {'name': False, 'format': False, 'size': False}
-        self.search_modified_images = False 
+        self.search_modified_images = False
         self.modified_images_properties = {1: True, 2: True, 3: True, 4: True, 5: True, 6: True, 7: True}
         self.comparison_method = comparison_method
         self.folder_for_move = None
         self.max_num_duplicates = 1000
         
     def find(self):
-        if self.specified_file:
-            return self.__find(self.specified_file)
         start = time.monotonic()
+        
+        file_is_specified = False
+        check_i = 0
+        
+        if self.specified_file:
+            file_is_specified = True
+            path_check_img = self.specified_file
+            curr_i = 0
+        else:
+            curr_i = 1
+        
         paths_images = self.files
         comparison_method = self.comparison_method
         
-        check_i = 0
-        curr_i = 1
         duplicate_count = 0
 
         while check_i < len(paths_images):
-            if paths_images[check_i] is not None:
-                checked_obj = ComparisonObject(paths_images[check_i], comparison_method)
+            if not file_is_specified:
+                path_check_img = paths_images[check_i]
+                
+            if path_check_img is not None:
+                checked_obj = ComparisonObject(path_check_img, comparison_method)
+            else:
+                check_i += 1
+                continue
                     
             while curr_i < len(paths_images):
                 path_curr_img = paths_images[curr_i]
-                if (check_i != curr_i) and (path_curr_img is not None):
-                    if self.require_identical_properties:
-                        if not check_identical_properties(checked_obj.file_path, path_curr_img, self.identical_properties):
-                            curr_i += 1
-                            continue
-                    
-                    curr_obj = ComparisonObject(path_curr_img, comparison_method)
-
-                    if self.__is_duplicates(checked_obj.comparison_data, curr_obj.comparison_data) or \
-                        (self.search_modified_images and self.__check_modified(checked_obj, curr_obj)):
-                        self.__action_with_duplicates(checked_obj, curr_obj)
-                        del paths_images[curr_i]
-                        duplicate_count += 1
-                        if duplicate_count >= self.max_num_duplicates: break
-                    else:
+                
+                if (path_check_img == path_curr_img) or (path_curr_img is None):
+                    curr_i += 1
+                    continue
+                
+                if self.require_identical_properties:
+                    if not check_identical_properties(checked_obj.file_path, path_curr_img, self.identical_properties):
                         curr_i += 1
-                        
-            if duplicate_count >= self.max_num_duplicates: break
+                        continue
+                
+                curr_obj = ComparisonObject(path_curr_img, comparison_method)
+                
+                if self.__is_duplicates(checked_obj.comparison_data, curr_obj.comparison_data) or \
+                    (self.search_modified_images and self.__check_modified(checked_obj, curr_obj)):
+                    self.__action_with_duplicates(checked_obj, curr_obj)
+                    if file_is_specified:
+                        curr_i += 1
+                    else:
+                        del paths_images[curr_i]
+                    duplicate_count += 1
+                    if duplicate_count >= self.max_num_duplicates: break
+                else:
+                    curr_i += 1
+                    
+            if file_is_specified or duplicate_count >= self.max_num_duplicates: break
             check_i += 1
             curr_i = check_i + 1
             
         print(duplicate_count, 'duplicates found')
         print(f'Script running time: {time.monotonic() - start}')
-     
-    def __find(self, specified_file: str):
-        start = time.monotonic()
-        paths_images = self.files
-        comparison_method = self.comparison_method
-        
-        duplicate_count = 0
-
-        if specified_file is not None:
-            checked_obj = ComparisonObject(specified_file, comparison_method)
-                    
-        for curr_img in paths_images:
-            if (specified_file != curr_img) and (curr_img is not None):
-                if self.require_identical_properties:
-                    if not check_identical_properties(checked_obj.file_path, curr_img, self.identical_properties):
-                        continue
-                    
-                curr_obj = ComparisonObject(curr_img, comparison_method)
-
-                if self.__is_duplicates(checked_obj.comparison_data, curr_obj.comparison_data) or \
-                    (self.search_modified_images and self.__check_modified(checked_obj, curr_obj)):
-                    self.__action_with_duplicates(checked_obj, curr_obj)
-                    duplicate_count += 1
-                    if duplicate_count >= self.max_num_duplicates: break
-            
-        print(duplicate_count, 'duplicates found')
-        print(f'Script running time: {time.monotonic() - start}')
-        
+    
     def __find_percentage_difference(self, data1, data2):
         method = self.comparison_method
         match method.name:
@@ -112,7 +106,7 @@ class DuplicatesFinder:
         # find the percentage difference
         diff = self.__find_percentage_difference(data1, data2)
         return diff <= (100 - self.comparison_method.similarity)
-        
+    
     def __check_modified(self, obj: ComparisonObject, obj_to_mod: ComparisonObject):
         properties = self.modified_images_properties
         img_to_mod = copy(obj_to_mod.object)
@@ -136,4 +130,3 @@ class DuplicatesFinder:
         name_curr_img = os.path.basename(path_curr_img)
         if self.folder_for_move:
             Path(path_curr_img).rename(f"{self.folder_for_move}/{name_curr_img}")
-        
