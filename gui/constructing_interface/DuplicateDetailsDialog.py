b870249d06912ee644c2fcaa0239a8490deb0e23
xeniaTa <kseniatarsenko1@gmail.com>
2024-07-01 17:00:41 +0300

    add delete file

--- gui/constructing_interface/DuplicateDetailsDialog.py
+++ gui/constructing_interface/DuplicateDetailsDialog.py
@@ -1,10 +1,12 @@
 from PyQt5.QtWidgets import QDialog, QVBoxLayout, QLabel, QPushButton, QHBoxLayout, QListWidget, QWidget, \
-    QListWidgetItem
+    QListWidgetItem, QMessageBox
 from PyQt5.QtGui import QPixmap, QFont
 from PyQt5.QtCore import Qt
 from datetime import datetime
 import os
-
+from pathlib import Path
+from random import randint
+from send2trash import send2trash
 
 class DuplicateDetailsDialog(QDialog):
     def __init__(self, file_path, duplicates, parent=None):
@@ -89,4 +91,10 @@ class DuplicateDetailsDialog(QDialog):
         pass
 
     def delete_file(self, file_path):
-        pass
+        if os.path.isfile(file_path):
+            path_to_delete = file_path.replace("/", "\\")
+            send2trash(path_to_delete)
+        else:
+            QMessageBox.warning(self, "Error occurred",
+                                "<h3>File doesn\'t exist</h3>"
+                                "The file may have already been deleted.<br/><br/>")  
