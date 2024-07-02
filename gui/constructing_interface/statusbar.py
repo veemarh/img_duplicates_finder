c6e415b899ab1297473f51c95f97282432bede74
veta <vee.marh.my@gmail.com>
2024-07-02 12:24:36 +0300

    statusbar constant message deleted

--- gui/constructing_interface/statusbar.py
+++ gui/constructing_interface/statusbar.py
@@ -1,10 +1,6 @@
-from PyQt5.QtWidgets import QStatusBar, QLabel
+from PyQt5.QtWidgets import QStatusBar
 
 
 def create_status_bar(main_window):
     statusbar = QStatusBar(main_window)
-    constant_message = "Constant"
-    constant_message_label = QLabel(f"{constant_message}")
-    statusbar.addPermanentWidget(constant_message_label)
-
     return statusbar
